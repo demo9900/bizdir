96a712238cf95ee8b941f932b0956410fbadfb9a
digitalkoncept <digitalkoncept@outlook.com>
2024-04-10 14:00:56 +0530

    nothing special

--- next.config.js
+++ next.config.js
@@ -3,15 +3,6 @@ const nextConfig = {
   experimental:{
     appDir: true
   },
-  async redirects() {
-    return [
-      {
-        source: '/admin/:path*',
-        destination: 'admin-bizdir.vercel.app/:path*',
-        permanent: false, // Set this to true if you want permanent redirects (HTTP 301)
-      },
-    ]
-  },
   
   reactStrictMode: true,
 }
